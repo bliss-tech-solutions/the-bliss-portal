21d2d255a9b323a5b0096a543ccb12f1551809ae
webtesting22 <developer.bliss@gmail.com>
2026-02-02 19:06:31 +0530

    Create ExecutionDashboard.jsx

---
+++ src/components/RoutesComponents/Dashboards/ExecutionDashboard/ExecutionDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ExecutionDashboard.css";
+
+const ExecutionDashboard = () => {
+    return (
+        <div>
+            <h1>Execution Dashboard</h1>
+        </div>
+    );
+};
+
+export default ExecutionDashboard;
